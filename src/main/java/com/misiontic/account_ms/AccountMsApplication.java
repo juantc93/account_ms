3f3e888cca3dd5b3718d03beda53a06a7c87cc5e
juantc93 <52570565+juantc93@users.noreply.github.com>
2021-07-29 19:30:05 -0500

    Nuevas clases y repositorios

--- src/main/java/com/misiontic/account_ms/AccountMsApplication.java
+++ src/main/java/com/misiontic/account_ms/AccountMsApplication.java
@@ -2,12 +2,24 @@ package com.misiontic.account_ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class AccountMsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountMsApplication.class, args);
 	}
 
+	@GetMapping
+	public List<Persona> hello(){
+		return List.of(	new Persona("1", "Camilo Garcia", "gcgarciab@unal.edu.co", 28));
+
+	}
 }
+
